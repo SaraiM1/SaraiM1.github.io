e1b7d297e47a47dd35f5e79984ee961d195be870
SaraiM1 <sarai.mateo@sjsu.edu>
2022-03-17 08:34:28 -0700

    diyps update

--- diyps2021/p5js-temp-diyps20213128097494808107906.js
+++ diyps2021/p5js-temp-diyps20213128097494808107906.js
@@ -8,8 +8,8 @@ function preload() {
 // preload() runs once, it may make you wait
 //  img = loadImage('cat.jpg');  // cat.jpg needs to be next to this .js file
 // you can link to an image on your github account
-  img = loadImage('https://dma-git.github.io/images/74.png');
-  img2 = loadImage('star_o.png');
+  //img = loadImage('https://dma-git.github.io/images/74.png');
+  img2 = loadImage('https://saraim1.github.io/diyps2021/star_o.png');
 
 }
 
@@ -99,8 +99,8 @@ function newkeyChoice(toolChoice) { //toolchoice is the key that was pressed
     strokeWeight(20);
     fill(random(255), random(255), random(255), random(255));
     rect(mouseX, mouseY, 50, 50);
-  } else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
-    image(img, mouseX-30, mouseY-30);
+  //} else if (toolChoice == 'g' || toolChoice == 'G') { // g places the image we pre-loaded
+    //image(img, mouseX-30, mouseY-30);
     
   } else if (toolChoice == 's' || toolChoice == 'S') { // s places the image we pre-loaded
     image(img2, mouseX, mouseY);
